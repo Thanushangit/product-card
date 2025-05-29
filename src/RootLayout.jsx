aba95116566816855733c74e0ada035f327d451e
Thanushan <sinnarasathanushan2002@gmail.com>
2025-05-29 17:35:24 +0530

    finished

--- src/RootLayout.jsx
+++ src/RootLayout.jsx
@@ -15,7 +15,7 @@ const RootLayout = () => {
       <Navbar onSearch={Handle} />
       <Outlet context={{ sedata }} />
 
-      <footer className='mb-2 text-gray-600  border-t-2 border-gray-200 pt-2 mt-8 flex items-center justify-center gap-1'>
+      <footer className='mb-2 text-gray-600 pt-2 mt-8 flex items-center justify-center gap-1 '>
         copyright &copy;{new Date().getFullYear()} All rights reserved S.Thanushan
         <Link to="https://github.com/Thanushangit/product-card" title='Soure Code' target='_blank'>
           <p className='hover:text-blue-500 transition-all duration-300'><FaGithub /></p>
